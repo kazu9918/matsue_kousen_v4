58a50e5d571bbaf8c41d3339c5aec03fd6bc66aa
Kazuya <kazuya.mikami@relic.co.jp>
2023-11-16 08:25:47 +0900

    siteコントローラーをhomeコントローラーに修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'site/index'
-	root 'site#index'
+  get 'home/index'
+	root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'monitoring' => 'monitoring#show'
