24806de02f7d8e503aa4f1ca14b79e1e57ffd2ac
Kazuya <kazuya.mikami@relic.co.jp>
2024-01-11 00:04:24 +0900

    10分間の平均値処理

--- app/controllers/monitoring_data_controller.rb
+++ app/controllers/monitoring_data_controller.rb
@@ -11,8 +11,22 @@ class MonitoringDataController < ApplicationController
   end
 
   def temperature_data
-    @temperature_data = MonitoringDatum.select(:recorded_at, :temperature)
-    render json: @temperature_data
+    first_record = MonitoringDatum.order(:recorded_at).first
+    last_record = MonitoringDatum.order(:recorded_at).last
+    return render json: [] unless first_record && last_record
+  
+    start_time = first_record.recorded_at
+    end_time = last_record.recorded_at
+    averages = []
+  
+    while start_time < end_time
+      next_time = start_time + 10.minutes
+      average_temperature = MonitoringDatum.where('recorded_at >= ? AND recorded_at < ?', start_time, next_time).average(:temperature)
+      averages.push({ period_start: start_time, period_end: next_time, average_temperature: average_temperature })
+      start_time = next_time
+    end
+  
+    render json: averages
   end
 
   def humidity_data
--- db/seeds/monitoring_datum.rb
+++ db/seeds/monitoring_datum.rb
@@ -1,8 +1,8 @@
-50.times do
+50.times do |i|
   MonitoringDatum.create(
     temperature: rand(-10.0..40.0),
     humidity: rand(0.0..100.0),
     lux: rand(0.0..1000.0),
-    recorded_at: rand(1.year).seconds.ago
+    recorded_at: 50.minutes.ago + i.minutes
   )
 end
